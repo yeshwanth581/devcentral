89d334da6b29ff0e2debdc289c1e862e48d8ad64
Yeshwanth Ragahvendra <yeshwanthraghavendra@gmail.com>
2020-09-21 11:14:45 +0530

    data for action cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Col } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 const Card = (props) => {
-  const { heading, description, tags } = props;
+  const { heading, description, tags = [], path } = props;
+  const history = useHistory();
+
+  const tagsList = tags.map((tag) => (
+    <span key={tag} className="tag">
+      {tag}
+    </span>
+  ));
+
   return (
-    <Col xs={12} sm={12} md={6} lg={4} xl={4}>
-      <div className="card-container">
-        <div className="card-heading">Heading</div>
-        <div className="card-description">
-          Desctiption text is being shown here.Please display test.
-        </div>
-        <div className="card-tags">
-          <span className="tag">test1</span>
-          <span className="tag">test1</span>
-          <span className="tag">test1</span>
-        </div>
+    <Col xs={12} sm={12} md={6} lg={6} xl={4}>
+      <div
+        key={heading}
+        className="card-container"
+        onClick={() => history.push(path)}
+      >
+        <div className="card-heading">{heading}</div>
+        <div className="card-description">{description}</div>
+        <div className="card-tags">{tagsList}</div>
       </div>
     </Col>
   );
--- src/components/HomeActionButtons.js
+++ src/components/HomeActionButtons.js
@@ -1,18 +1,46 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import Card from "./Card";
 const HomeActionButtons = () => {
+  const actionList = [
+    {
+      heading: "Learn",
+      description:
+        "List of learning paths which has a step by step guide to become a master of your stack.",
+      tags: ["FullStack", "FE", "BE", "Devops", "AI/ML"],
+      path: "/learn"
+    },
+    {
+      heading: "Mentor/Buddy",
+      description:
+        "Find or become a mentor. Buddy gets guided by a mentor to become a better developer.",
+      tags: ["Mentor", "Trainee", "Help each other"],
+      path: "/about"
+    },
+    {
+      heading: "Interview",
+      description:
+        "Give a mock interview with experts and peers. Get your constructive feedback and improve.",
+      tags: ["Tech Interview", "Coding", "Design"],
+      path: "/interviews"
+    }
+  ];
+
+  const actionItems = actionList.map((i) => (
+    <Card
+      key={i.heading}
+      heading={i.heading}
+      description={i.description}
+      path={i.path}
+      tags={i.tags}
+    />
+  ));
+
   return (
     <div className="homeActionButtons">
       <Container fluid>
         <p className="section-heading">Pick a category to get started</p>
-        <Row className="cardsList">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </Row>
+        <Row className="cardsList">{actionItems}</Row>
       </Container>
     </div>
   );
